518117bb1464240a961fad56ec487104871644c3
Junaid Peer <junaid@Junaids-MacBook-Pro.local>
2024-02-07 16:38:24 +0000

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  p-24">
       <div>
-        <p>Check eligibility for different Solana airdrops</p>
+        <p></p>
         <h1 className=" font-semibold text-[3.75rem] ">
           Web portfolio tracker
         </h1>
